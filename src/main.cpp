3277891e4c820d06c9cd200a7cf5687bef80de0f
Abdeblary <mahmoud.abdelbaty@gmail.com>
2020-06-22 13:43:52 +0000

    adding reading from files and append to files

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include "controller.h"
 #include "game.h"
 #include "renderer.h"
-
+#include <fstream>
+#include <sstream>
 int main() {
  
 
@@ -10,8 +11,48 @@ int main() {
   Controller controller;
   Game game(kGridWidth, kGridHeight);
   game.Run(controller, renderer, kMsPerFrame);
+  
+  //add current score to score file
+  std::ofstream out("scores.txt",std::ios_base::app);
+  if(out.is_open())
+  {
+  out << game.GetScore();
+  out << "\n\r";
+  out.close();
+  }
+  else
+  {
+    std::cout<<"error openning score file\n";
+  }
+  
+  
+  //get the higest score
+  std::ifstream stream("scores.txt");
+  std::istringstream linestream;
+  int highestScore = 0;
+  std::string line="";
+  if(stream.is_open())
+  {
+    int score{0};
+
+    while(std::getline(stream,line))
+    {
+       linestream.str(line);
+       linestream>>score;
+       if(score > highestScore)
+        highestScore = score;
+    };
+
+    stream.close();
+  }
+  else 
+  {
+        std::cout<<"error openning score file\n";
+  }
   std::cout << "Game has terminated successfully!\n";
   std::cout << "Score: " << game.GetScore() << "\n";
   std::cout << "Size: " << game.GetSize() << "\n";
+  std::cout << "Highest Score: " << highestScore << "\n";
+
   return 0;
 }
